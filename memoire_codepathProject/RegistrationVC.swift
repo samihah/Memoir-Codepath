0e389721d2c74fd5afca59a961b61bb434f78b3f
Nicholas Naudé <nicnaude@Nicholass-MacBook-Pro.local>
2016-11-19 22:27:42 -0800

    Refactors storyboards.

---
+++ memoire_codepathProject/RegistrationVC.swift
@@ -0,0 +1,35 @@
+//
+//  RegistrationVC.swift
+//  memoire_codepathProject
+//
+//  Created by Nicholas Naudé on 19/11/2016.
+//  Copyright © 2016 Samihah Azim. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
